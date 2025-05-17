1d268d46df8cb0204a657d4962dd316ff1966c24
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2025-05-17 17:12:27 +0200

    updating Route in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import About from './pages/About';
+import Footer from './components/Footer';
+import Landing from './pages/Landing';
+import Navbar from './components/Navbar';
+
+
 
 const App: React.FC = () => {
   return (
+    <BrowserRouter>
     <div>
       <About />
+      <Landing />
+      <Footer />
+      <Navbar />
+
+      
     </div>
+    </BrowserRouter>
   );
 };
 
