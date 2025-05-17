d8f680ed3eac53a513693492e34baf529ddaba03
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2025-05-17 16:53:45 +0200

    creating landing page

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Project</h1>
+      <p>This is the landing page.</p>
+    </div>
+  );
+};
+
+export default Landing;
