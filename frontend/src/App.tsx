4bacd5a9aba5c19efeb1ee2cea63760977950e77
nikita150187 <nikita.taware@stud.sundsgarden.se>
2025-05-20 13:08:36 +0530

    Resolved merge conflict in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,34 @@
-<<<<<<< HEAD
+import React from 'react';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 import AboutUs from "./pages/AboutUs";
 import AboutSection from "./pages/AboutSection";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
         <Route path="/about" element={<AboutUs />} />
         <Route path="/about/:section" element={<AboutSection />} />
+
         <Route path="*" element={<div>Page not found</div>} />
       </Routes>
+      <Footer />
     </Router>
   );
-=======
-import React from 'react';
-import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './pages/About';
-
-import Landing from './pages/Landing';
-import Navbar from './components/Navbar';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Footer from './components/Footer';
-
-
-
-const App: React.FC = () => {
- 
-    return (
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    );
->>>>>>> a082947708db96ca586bda724db0b725c843103b
 };
 
 export default App;
