275147543e05ba5540e0a05fd785222aad64cfcd
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2025-05-20 15:12:57 +0200

    adding welcome massage for users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import './index.css';
+<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
+=======
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserProvider } from './context/UserContext';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+>>>>>>> 1e0301c (adding welcome user massage)
 
 import Landing from './pages/Landing';
 import Navbar from './components/Navbar';
@@ -10,6 +18,7 @@ import Register from './pages/Register';
 import Footer from './components/Footer';
 
 
+<<<<<<< HEAD
 
 const App: React.FC = () => {
  
@@ -26,6 +35,24 @@ const App: React.FC = () => {
         <Footer />
       </BrowserRouter>
     );
+=======
+const App = () => {
+  return (
+    <UserProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/about/:section" element={<AboutSection />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </UserProvider>
+  );
+>>>>>>> 1e0301c (adding welcome user massage)
 };
 
 export default App;
