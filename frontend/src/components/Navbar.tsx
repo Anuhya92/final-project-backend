f42708cf0b9bf202d186ba8ad6c9b1af96300a50
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2025-05-22 12:12:00 +0200

    added webshop to the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -14,10 +14,22 @@ const Navbar: React.FC = () => {
       />
 
       {/* Links */}
-      <div className="flex items-center space-x-8 text-lg" style={{ color: '#E5A000' }}>
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/login" className="hover:underline">Login</Link>
-        <Link to="/register" className="hover:underline">Register</Link>
+      <div
+        className="flex items-center space-x-8 text-lg"
+        style={{ color: "#E5A000" }}
+      >
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/webshop" className="hover:underline">
+          Webshop
+        </Link>
+        <Link to="/login" className="hover:underline">
+          Login
+        </Link>
+        <Link to="/register" className="hover:underline">
+          Register
+        </Link>
 
         {/* Dropdown for about us section*/}
         <div className="relative">
@@ -26,11 +38,41 @@ const Navbar: React.FC = () => {
           </button>
           {open && (
             <div className="absolute left-0 mt-2 bg-white text-black rounded shadow-md p-2 z-50 w-56">
-              <Link to="/about" onClick={() => setOpen(false)} className="block px-2 py-1 hover:bg-yellow-400">Overview</Link>
-              <Link to="/about/philosophy" onClick={() => setOpen(false)} className="block px-2 py-1 hover:bg-yellow-400">Our Philosophy</Link>
-              <Link to="/about/sustainability" onClick={() => setOpen(false)} className="block px-2 py-1 hover:bg-yellow-400">Sustainability Work</Link>
-              <Link to="/about/contact" onClick={() => setOpen(false)} className="block px-2 py-1 hover:bg-yellow-400">Contact Us</Link>
-              <Link to="/about/team" onClick={() => setOpen(false)} className="block px-2 py-1 hover:bg-yellow-400">Team Behind the App</Link>
+              <Link
+                to="/about"
+                onClick={() => setOpen(false)}
+                className="block px-2 py-1 hover:bg-yellow-400"
+              >
+                Overview
+              </Link>
+              <Link
+                to="/about/philosophy"
+                onClick={() => setOpen(false)}
+                className="block px-2 py-1 hover:bg-yellow-400"
+              >
+                Our Philosophy
+              </Link>
+              <Link
+                to="/about/sustainability"
+                onClick={() => setOpen(false)}
+                className="block px-2 py-1 hover:bg-yellow-400"
+              >
+                Sustainability Work
+              </Link>
+              <Link
+                to="/about/contact"
+                onClick={() => setOpen(false)}
+                className="block px-2 py-1 hover:bg-yellow-400"
+              >
+                Contact Us
+              </Link>
+              <Link
+                to="/about/team"
+                onClick={() => setOpen(false)}
+                className="block px-2 py-1 hover:bg-yellow-400"
+              >
+                Team Behind the App
+              </Link>
             </div>
           )}
         </div>
@@ -40,7 +82,3 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
-
-
-
-
